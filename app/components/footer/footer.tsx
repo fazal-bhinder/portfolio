020939ac188749a6c08c2ec3310e02da4c8177bd
fazal-bhinder <163873947+fazal-bhinder@users.noreply.github.com>
2025-07-01 00:33:18 +0530

    minor changes

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -56,7 +56,7 @@ export function Footer() {
                 <span className="sr-only">GitHub</span>
               </Link>
               <Link
-                href="https://x.com/bhinder__fazal"
+                href="https://x.com/damnfazal"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-muted-foreground hover:text-foreground transition-colors"
